1030fac33befa6276b75d55ec48e7065eed85d82
Brindha1314 <124461430+Brindha1314@users.noreply.github.com>
2023-02-21 00:27:53 +0530

    day1.assignment1

---
+++ Computer.java
@@ -0,0 +1,9 @@
+package week3.day1;
+
+public class Computer {
+	public void computermodel(String a)
+	{
+		System.out.println("The Printer Model is: latitude 3490");
+
+}
+}
---
+++ Desktop.java
@@ -0,0 +1,16 @@
+package week3.day1;
+
+public class Desktop extends Computer {
+
+	public void computersize(String b)
+	{
+		System.out.println("The Printer Size is: 15");
+	}
+	public static void main(String[] args)
+	{
+		Desktop ds=new Desktop();
+				ds.computersize("String b");
+		ds.computermodel("String a");
+	}
+}
+
